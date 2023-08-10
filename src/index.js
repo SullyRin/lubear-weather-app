b992251d2b48541a2f02a81d616211b7b357ecbc
SullyRin <zecherach@gmail.com>
2023-08-10 17:25:12 +0100

    renamed because path was wrong

--- src/index.js
+++ src/index.js
@@ -1,40 +1,100 @@
-let weather = {
-  gridania: {
-    temp: 19.7,
-    humidity: 80,
-  },
-  "mor dhona": {
-    temp: 17.3,
-    humidity: 50,
-  },
-  "limsa lominsa": {
-    temp: 30.2,
-    humidity: 20,
-  },
-  ishgard: {
-    temp: -5,
-    humidity: 20,
-  },
-};
+function formatDate(date) {
+  let year = date.getFullYear();
 
-let city = prompt("Enter a city");
-let cityLowerCase = city.toLowerCase();
+  let hours = date.getHours();
+  let minutes = date.getMinutes().toString().padStart(2, "0");
 
-if (
-  city === "Gridania" ||
-  city === "Mor Dhona" ||
-  city === "Limsa Lominsa" ||
-  city === "Ishgard"
-) {
-  alert(
-    `It is currently ${Math.round(
-      weather[cityLowerCase].temp
-    )}°C in ${city} with a humidity of ${Math.round(
-      weather[cityLowerCase].humidity
-    )}`
-  );
-} else {
-  alert(
-    `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
-  );
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
+
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[date.getMonth()];
+
+  return `${day} ${month} ${date.getDate()} ${year}, ${hours}:${minutes} Eorzea Time`;
 }
+
+let now = new Date();
+let formattedDate = formatDate(now);
+let timeAndDate = document.querySelector("#time-and-date");
+timeAndDate.innerHTML = formattedDate;
+
+function displayWeather(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let description = response.data.weather[0].description;
+  //  console.log(`Temperature received: ${temperature}°C`);
+  let displayTemperature = document.querySelector("#temperature");
+  let displayDescription = document.querySelector("#weather-condition");
+
+  displayTemperature.innerHTML = `${temperature}°`;
+  displayDescription.innerHTML = description;
+}
+
+function cityInput(event) {
+  event.preventDefault();
+  let cityNameInput = document.querySelector("#city-input");
+  let cityName = cityNameInput.value;
+  let cityNameElement = document.querySelector("#city-name");
+  cityNameElement.innerHTML = cityName;
+
+  let apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
+  let city = cityName;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+  axios.get(url).then(displayWeather);
+}
+
+let changeCity = document.querySelector("#city-form");
+changeCity.addEventListener("submit", cityInput);
+
+function handlePosition(position) {
+  // console.log(position.coords.latitude);
+  // console.log(position.coords.longitude);
+
+  let lat = Math.round(position.coords.latitude);
+  let lon = Math.round(position.coords.longitude);
+  let apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+
+  axios.get(url).then((response) => {
+    let cityNameElement = document.querySelector("#city-name");
+    let temperatureElement = document.querySelector("#temperature");
+    let descriptionElement = document.querySelector("#weather-condition");
+
+    cityNameElement.innerHTML = response.data.name;
+    temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°`;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+  });
+}
+function getCurrentLocation() {
+  navigator.geolocation.getCurrentPosition(handlePosition);
+}
+
+let currentPositionButton = document.querySelector("#current-location");
+currentPositionButton.addEventListener("click", getCurrentLocation);
+
+// function changeTempUnit(event) {
+//  event.preventDefault();
+//  let TempInF = document.querySelector("#c-to-f");
+
+//  if (TempInF.textContent === "18°") {
+//    TempInF.innerHTML = "64°";
+//  } else {
+//    TempInF.innerHTML = "18°";
+//  }
+// }
+
+// let convertToFahrenheit = document.querySelector("#temperature");
+// convertToFahrenheit.addEventListener("click", changeTempUnit);
